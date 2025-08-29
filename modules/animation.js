2756c61cb0be2c1e5f9ad40fb2c1b164aa7de921
Daria <kordesovad@gmail.com>
2025-08-29 13:24:11 +0200

    add animation: input text shadow

--- modules/animation.js
+++ modules/animation.js
@@ -1,15 +1,30 @@
+let leftInput = document.getElementById('playerX_name');
+let leftSpanPH = document.querySelector('.fp-l');
+let rightInput = document.getElementById('player0_name');
+let rightSpanPH = document.querySelector('.fp-r');
 
 
 let animation = {
     inputAnimation() {
-        console.log("input animation");
+        function syncRight() {
+            if (rightInput.value.trim() === "") {
+              rightSpanPH.classList.add("input-empty");
+            } else {
+              rightSpanPH.classList.remove("input-empty");
+            }
+        }
+        function syncLeft() {
+            if (leftInput.value.trim() === "") {
+              leftSpanPH.classList.add("input-empty");
+            } else {
+              leftSpanPH.classList.remove("input-empty");
+              syncRight();
+            }
+        }
+        syncLeft();
+        leftInput.addEventListener("input", syncLeft);
+        rightInput.addEventListener("input", syncRight);
     }
 }
-
-//получаем два инпута
-//анимируем левый - до тех пор пока он не будет заполнен
-//потом сразу анимируем правый - до тех пор пока он не будет заполнен
-
-//пока что анимация будет заключаться в том что текст placeholder-а будет мигать красным
 
 export default animation;
